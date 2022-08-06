e3d613455f013d74260c41e07f5cdc63637a5b28
Brad Runnels <runnels.brad@gmail.com>
2022-08-06 19:59:00 -0400

    init commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <meta name='viewport' content='width=device-width,initial-scale=1' />
+                <link rel='icon' href='/favicon.ico' sizes='any' />
+                <link rel='icon' href='/icon.svg' type='image/svg+xml' />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+};
