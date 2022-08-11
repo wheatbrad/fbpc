b90228e667397e9ef2dcb37c5824656181b92cc1
Brad Runnels <runnels.brad@gmail.com>
2022-08-10 20:14:46 -0400

    rename prisma client module to differentiate between prisma module

--- src/utils/dbConnector.ts
+++ src/utils/dbConnector.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from '@prisma/client';
 
 declare global { var prisma: PrismaClient | undefined }
 
-export const prisma = global.prisma || new PrismaClient();
+export const dbClient = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
