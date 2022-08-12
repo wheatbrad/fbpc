bf8130511c53471c64377053e459bc56ee90e45d
Brad Runnels <runnels.brad@gmail.com>
2022-08-12 18:58:11 -0400

    trpc query for player name on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { trpc } from '~/utils/trpc';
 
 const Home: NextPage = () => {
+    const player = trpc.useQuery(['getPlayerById', 1]);
+
     return (
         <>
             <Head>
@@ -9,6 +12,7 @@ const Home: NextPage = () => {
                 <meta name="description" content="The Runnels, Reeder, Krueger, and Trehern families vie for the annual college football bowl pick'em championship." />
             </Head>
             <h1>2022 Family Bowl Pick'em</h1>
+            <p>{`Welcome ${player.data?.firstName || 'player'}, ready to make your picks?`}</p>
         </>
     );
 };
