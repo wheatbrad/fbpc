c0942cd9416f8a8505d85a657d9e5167f2b28a4e
Brad Runnels <runnels.brad@gmail.com>
2022-08-12 21:03:46 -0400

    set player first name with ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage, PreviewData } from 'next';
 import Head from 'next/head';
-import { trpc } from '~/utils/trpc';
+import { ParsedUrlQuery } from 'querystring';
+import { dbClient } from '~/utils/dbConnector';
 
-const Home: NextPage = () => {
-    const player = trpc.useQuery(['getPlayerById', 1]);
+interface Player {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+
+const Home: NextPage<Player> = (player) => {
 
     return (
         <>
@@ -12,9 +19,21 @@ const Home: NextPage = () => {
                 <meta name="description" content="The Runnels, Reeder, Krueger, and Trehern families vie for the annual college football bowl pick'em championship." />
             </Head>
             <h1>2022 Family Bowl Pick'em</h1>
-            <p>{`Welcome ${player.data?.firstName || 'player'}, ready to make your picks?`}</p>
+            <p>{`Welcome ${player?.firstName || 'player'}, ready to make your picks?`}</p>
         </>
     );
 };
 
+
 export default Home;
+
+export const getStaticProps: GetStaticProps<Player, ParsedUrlQuery, PreviewData> = async () => {
+    const player = await dbClient.player.findUnique({ where: { id: 1}});
+
+    return { props: {
+        id: player?.id,
+        firstName: player?.firstName,
+        lastName: player?.lastName,
+        email: player?.email
+    } as Player };
+};
